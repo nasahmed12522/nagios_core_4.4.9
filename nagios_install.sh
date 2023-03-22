de24716f356ed23f6452f6eb265d90528e779e18
Nasim Ahmed <127286119+nasahmed12522@users.noreply.github.com>
2023-03-22 12:53:42 +0600

    Update nagios_install.sh

--- nagios_install.sh
+++ nagios_install.sh
@@ -1,38 +1,71 @@
-sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+## Disable SELinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled' /etc/selinux/config
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+cat /etc/selinux/config
 setenforce 0
 getenforce
-yum install -y epel-release git
-yum install -y gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils unzip httpd php gd gd-devel perl postfix
+
+## Configure EPEL Repository
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm 
+yum install -y git gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils unzip httpd php gd gd-devel perl postfix
+yum install -y perl-Net-SNMP
 cd /tmp
 git clone https://github.com/nasahmed12522/nagios_core_4.4.9.git
-useradd nagios
+
+
+## Create Nagios user and group
 useradd apache
 groupadd nagcmd
-usermod -a -G nagcmd nagios
-usermod -a -G nagcmd apache
+groupadd nagios
+useradd -g nagios -G nagcmd nagios
+usermod -aG nagcmd apache
+
+
+## Change directory path into extracted package and installation
+
 cd /tmp/nagios_core_4.4.9/
 tar xzf nagios-4.4.9.tar.gz; tar xzf nagios-plugins.tar.gz; tar xzf nagios-plugins-2.3.3.tar.gz 
 cd /tmp/nagios_core_4.4.9/nagios-4.4.9/
-./configure
+./configure --with-command-group=nagcmd
 make all
 make install
 make install-init
 make install-commandmode
 make install-config
+make install-exfoliation
 make install-webconf
-firewall-cmd --zone=public --add-port=80/tcp
-firewall-cmd --zone=public --add-port=80/tcp --permanent
+
+
+## Add Firewall rule to allow http and https
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
 firewall-cmd --reload
+
+## Enable and start httpd & nagios services
 systemctl start httpd.service
-systemctl start nagios.service
+systemctl enable httpd.service
+systemctl start nagios
+systemctl enable nagios
+
+## Verify the Nagios config
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
 cd /tmp/nagios_core_4.4.9/nagios-plugins-2.3.3
-./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
-make all
+
+./configure --with-nagios-user=nagios --with-nagios-group=nagcmd
+make
+make install
+
+
+## Verify the plugins in below path
+cd /usr/local/nagios/libexec/
+ls
+
 service nagios restart
 cd /tmp/nagios_core_4.4.9/nagios-plugins-release-2.3.3/
 ./tools/setup
 chown nagios:nagcmd /usr/local/nagios/var/rw/
 chown nagios:nagcmd /usr/local/nagios/var/rw/nagios.cmd
 systemctl restart nagios.service
-systemctl enable httpd.service
+
 htpasswd -cb /usr/local/nagios/etc/htpasswd.users nagiosadmin n@123
